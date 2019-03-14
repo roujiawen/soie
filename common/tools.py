ebbb365d45531153461af23a3736c2a0ab65e752
roujiawen <wenroujia@gmail.com>
2019-03-13 19:37:55 -0700

    linted tools.py and added docstrings

--- common/tools.py
+++ common/tools.py
@@ -1,10 +1,14 @@
+"""This modeule contains useful functions that are shared across different
+UI components.
+"""
+
 import Tkinter as tk
 
 from common.styles import BUTTON_X_MARGIN
 
 
 def is_within(value, min_, max_):
-    """ Inclusive.
+    """ Return True if value is within [min_, max_], inclusive.
     """
     if value > max_:
         return False
@@ -12,27 +16,40 @@ def is_within(value, min_, max_):
         return False
     return True
 
+
 def fit_into(value, min_, max_):
-    return max(min_,min(max_, value))
+    """ Return value bounded by min_ and max_.
+    """
+    return max(min_, min(max_, value))
+
 
 def create_buttons(parent, button_dict):
     """Create a group of buttons at once.
 
-    Parameters
-    ----------
-    parent : Frame object
-    button_dict : dictionary
-        Following the format {name:[text, row, column], ...}. Command function
-        name is the same as button name.
+    Parameters:
+        parent (tk.Frame): The parent on which the button widget is anchored.
+        button_dict (dict): Specifies button display name and position. Given
+            in the format {name:[text, row, column], ...}. Command function
+            name should be the same as button name.
+
+    Returns:
+        buttons (dict): A group of button widgets created. Format is
+            {name: button_widget, ...}
     """
     buttons = {}
     for name, attributes in button_dict.items():
-        b = tk.Button(parent, text=attributes[0], command=getattr(parent,name), padx=BUTTON_X_MARGIN)
-        b.grid(row=attributes[1], column=attributes[2])
-        buttons[name] = b
+        # Associate the button with a command function
+        button = tk.Button(parent, text=attributes[0],
+                           command=getattr(parent, name), padx=BUTTON_X_MARGIN)
+        button.grid(row=attributes[1], column=attributes[2])
+        buttons[name] = button
     return buttons
 
+
 def counts2slices(counts):
+    """Convert a list of counts to cummulative slices.
+    Example: [5, 1, 3] ==> [slice(0, 5), slice(5, 6), slice(6, 9)]
+    """
     cumu = [sum(counts[:i]) for i in range(len(counts)+1)]
     slices = [slice(cumu[i-1], cumu[i]) for i in range(1, len(cumu))]
     return slices
