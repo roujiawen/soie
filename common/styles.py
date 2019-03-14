fd66f3f6efa84986bc7d1883e76576d07ac8db31
roujiawen <wenroujia@gmail.com>
2019-03-13 19:26:44 -0700

    linted style.py and added docstring

--- common/styles.py
+++ common/styles.py
@@ -1,25 +1,28 @@
-#General
+"""Stylesheet for the GUI. Set constants that define consistant fonts, colors,
+and margins for different UI components. """
+
+# General
 CELL_TYPE_HEADER_COLOR = "black"
 CELL_TYPE_HEADER_FONT = ("Helvetica", "12", "underline")
 CELL_TYPE_LABELS = ["Blue", "Red", "Green"]
 CELL_COLORS = ["#0000CD", "#DC143C", "#228B22"]
 BUTTON_X_MARGIN = 8
-#APP
+# APP
 APP_COLOR = "white"
-#PLOTS
-CELL_ALPHA = [0.8,0.7,0.6]
+# PLOTS
+CELL_ALPHA = [0.8, 0.7, 0.6]
 
-#SimsFrame
+# SimsFrame
 SIMS_FRAME_COLOR = "white"
 
-#GraphFrame
-ON_HOVER_COLOR = "#DCDCDC" #light grey
-ON_CLICK_COLOR = "#808080" #dark grey
+# GraphFrame
+ON_HOVER_COLOR = "#DCDCDC"  # light grey
+ON_CLICK_COLOR = "#808080"  # dark grey
 ON_SELECT_COLOR = "#1589C9"
-EVOLVING_BORDER_COLOR = "#FE5F55"#FE5D26  D62839
+EVOLVING_BORDER_COLOR = "#FE5F55"
 
-##########RIGHT FRAME###########
-#SimInfoFrame
+# ----------------RIGHT FRAME----------------
+# SimInfoFrame
 SIM_INFO_HEADER_SPACE = 5
 SIM_INFO_LEFT_SPACE = 8
 
@@ -28,20 +31,20 @@ HEADER_FONT = ('Helvetica', "12", "bold")
 HEADER_COLOR = "black"
 
 BODY_FONT = ('Helvetica', '12')
-BODY_COLOR = "black"#"#303030" #almost dark
+BODY_COLOR = "black"
 
-#AdvancedMutateFrame
+# AdvancedMutateFrame
 ADVANCED_MUTATE_FRAME_COLOR = "#E7E7E7"
 
-##########TOP FRAME#############
-#SelectFrame
+# ----------------TOP FRAME----------------
+# SelectFrame
 MUTATE_FRAME_COLOR = CROSS_FRAME_COLOR = INSERT_LIB_FRAME_COLOR = "white"
 H2_FONT = ("Helvetica", "14", "bold")
 H2_COLOR = "black"
 LG_BODY_FONT = ("Arila", "14")
 LG_BODY_COLOR = "black"
 
-###########EDIT WINDOW#########
+# ----------------EDIT WINDOW----------------
 EDIT_BODY_FONT = ('Helvetica', '10')
 EDIT_BODY_COLOR = "black"
 EDIT_COLOR_FONT = ("Helvetica", "10", "underline")
